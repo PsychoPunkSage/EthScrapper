83aa9fb3576939c7ec8d05765d5978fe44cfd997
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-08-16 16:28:27 +0530

    fix::> Added logic to store data in RedisDB

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
 	"github.com/joho/godotenv"
 )
 
@@ -50,8 +53,8 @@ func main() {
 	// Connect to Sepolia
 	client, err := ethclient.Dial(alchemyProjectId)
 	if err != nil {
-		fmt.Println("Error:: Failed to connect to Ethereum node")
-		fmt.Println("Error:: Check you Project ID (i.e. RPC-URL)")
+		fmt.Println("[ERROR]		Failed to connect to Ethereum node")
+		fmt.Println("[ERROR]		Check you Project ID (i.e. RPC-URL)")
 		fmt.Println(err)
 		return
 	}
@@ -72,4 +75,46 @@ func main() {
 		Addresses: []common.Address{address},
 		Topics:    [][]common.Hash{{topicHash}},
 	}
+
+	logs, err := client.FilterLogs(ctx, query)
+	if err != nil {
+		fmt.Println("[ERROR]		Failed to query logs")
+		fmt.Println(err)
+		return
+	}
+
+	// Store logs in Redis
+	index := 0
+	for _, vlogs := range logs {
+		block, err := client.BlockByHash(ctx, vlogs.BlockHash)
+		if err != nil {
+			log.Fatalf("[ERROR]		Failed to retrieve block: %v", err)
+		}
+
+		// Extract Data in question
+		data := EventData{
+			L1RootInfo: string(vlogs.Data),
+			Blocktime:  time.Unix(int64(block.Time()), 0),
+			ParentHash: block.ParentHash(),
+			LogIndex:   vlogs.Index,
+		}
+
+		// Serialize the data (Go Data ==> JSON)
+		serializedData, err := json.Marshal(data)
+		if err != nil {
+			log.Fatalf("[ERROR]        Failed to serialize data: %v", err)
+		}
+
+		// Store data in RedisDB
+		err = rdb.Set(ctx, strconv.Itoa(index), serializedData, 0).Err()
+		if err != nil {
+			log.Fatalf("[ERROR]        Failed to store data in Redis: %v", err)
+		}
+
+		index++
+	}
+
+	log.Println("|=================================|")
+	log.Println("| All events stored successfully. |")
+	log.Println("|=================================|")
 }
