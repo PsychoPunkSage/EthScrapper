90452c2b4a6da64c3d7c5397b07e9feb48aa4798
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-08-16 23:35:14 +0530

    fix::> seperated db side opr from main.go

---
+++ database/redisclient.go
@@ -0,0 +1,67 @@
+package database
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"strconv"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
+
+type TestData struct {
+	MsgData string `json:"msg"`
+	Data    uint   `json:"data"`
+}
+
+func ConnectToRedis(redisHost, redisPort, redisPassword string) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisHost + ":" + redisPort,
+		Password: redisPassword,
+		DB:       0,
+	})
+
+	return rdb
+}
+
+func TestDatabase(rdb *redis.Client) {
+	data := TestData{
+		MsgData: "test data",
+		Data:    42,
+	}
+
+	serializedData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalf("[ERROR]        Failed to serialize data: %v", err)
+	}
+
+	err = rdb.Set(ctx, strconv.Itoa(19), serializedData, 0).Err()
+	if err != nil {
+		log.Fatalf("[ERROR]        Failed to store data in Redis: %v", err)
+	}
+}
+
+func RetrieveRedisData(redisHost, redisPort, redisPassword string) {
+	// Connect to Redis
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisHost + ":" + redisPort,
+		Password: redisPassword,
+		DB:       0,
+	})
+
+	// Fetch and print all keys and values
+	keys, err := rdb.Keys(ctx, "*").Result()
+	if err != nil {
+		log.Fatalf("Failed to fetch keys: %v", err)
+	}
+
+	for _, key := range keys {
+		val, err := rdb.Get(ctx, key).Result()
+		if err != nil {
+			log.Fatalf("Failed to fetch value for key %s: %v", key, err)
+		}
+		log.Printf("Key: %s, Value: %s\n", key, val)
+	}
+}
