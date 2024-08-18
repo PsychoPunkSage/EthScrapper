cc04dba214fc7eb85daf9531f5da041a0e3221e7
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-08-18 23:35:30 +0530

    fix::> Littile Optimized version to make fast redis storage

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 	"math/big"
 	"os"
 	"strconv"
+	"sync" // CHANGED: Added for concurrency
 	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/go-redis/redis/v8"
 	"github.com/joho/godotenv"
@@ -29,7 +31,10 @@ type TestData struct {
 	Data    uint   `json:"data"`
 }
 
-var ctx = context.Background()
+var (
+	ctx = context.Background()
+	wg  sync.WaitGroup // CHANGED: Added for goroutine synchronization
+)
 
 func main() {
 	fmt.Println("Welcome to EthScrapper for Sepolia")
@@ -41,7 +46,6 @@ func main() {
 		return
 	}
 
-	// Get all the data from the .env file (all are #strings)
 	infuraProjectId := os.Getenv("INFURA_PROJECT_ID")
 	redisHost := os.Getenv("REDIS_HOST")
 	redisPort := os.Getenv("REDIS_PORT")
@@ -49,49 +53,43 @@ func main() {
 	contractAddress := os.Getenv("CONTRACT_ADDRESS")
 	topic := os.Getenv("TOPIC")
 
-	// Connect to Sepolia
 	client, err := ethclient.Dial("https://sepolia.infura.io/v3/" + infuraProjectId)
 	if err != nil {
 		log.Fatalf("[ERROR]		Failed to connect to Ethereum node\n")
-		log.Fatalf("[ERROR]		Check you Project ID (i.e. RPC-URL)\n")
 		fmt.Println(err)
 		return
 	}
 
-	chainid, err := client.ChainID(context.Background())
+	chainid, err := client.ChainID(ctx)
 	if err != nil {
 		log.Fatalf("[ERROR]		Failed to get ChainID: %v", err)
 	}
 	fmt.Printf("[INFO]		ChainID: %d\n", chainid.Int64())
 
-	// Check RPC connection (Print latest Block)
 	header, err := client.HeaderByNumber(ctx, nil)
 	if err != nil {
 		log.Fatalf("[ERROR]		Failed to get latest block: %v", err)
 	}
 	fmt.Printf("[INFO]		Latest block number: %d\n", header.Number.Uint64())
 
-	// Connect with Redis
+	// CHANGED: Redis client is created once and reused
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     redisHost + ":" + redisPort,
 		Password: redisPassword,
 		DB:       0,
 	})
 
-	// Test Redis Connection
 	TestDatabase(rdb)
-	retrieve(redisHost, redisPort, redisPassword)
+	retrieve(rdb)
 
-	// Topic & latest block number:
 	topicHash := common.HexToHash(topic)
 	address := common.HexToAddress(contractAddress)
 	blockNumberBig := big.NewInt(int64(header.Number.Uint64()))
 
-	// query logs
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{address},
 		Topics:    [][]common.Hash{{topicHash}},
-		FromBlock: new(big.Int).Sub(blockNumberBig, big.NewInt(100)), // Only 100 blocks considered
+		FromBlock: new(big.Int).Sub(blockNumberBig, big.NewInt(100)),
 		ToBlock:   blockNumberBig,
 	}
 
@@ -105,70 +103,66 @@ func main() {
 	fmt.Printf("[INFO]        	- related to Topic <%v>\n", topicHash)
 	fmt.Printf("[INFO]        	- in Contract Address <%v>\n", address)
 
-	// Store logs in Redis
+	// CHANGED: Define batch size for Redis pipeline
+	batchSize := 100
+	pipe := rdb.Pipeline() // CHANGED: Start a Redis pipeline
+	defer pipe.Close()
+
 	index := 0
 	for _, vlog := range logs {
-		// fmt.Printf("Log: %+v\n", vlog)
-		block, err := client.BlockByHash(ctx, vlog.BlockHash)
-		if err != nil {
-			log.Fatalf("[ERROR]		Failed to retrieve block: %v", err)
-		}
+		wg.Add(1)
+		go func(vlog types.Log, index int) { // CHANGED: Added goroutines for concurrent processing
+			defer wg.Done()
 
-		// Extract Data in question
-		data := EventData{
-			L1RootInfo: string(vlog.Data),
-			Blocktime:  time.Unix(int64(block.Time()), 0),
-			ParentHash: block.ParentHash(),
-			LogIndex:   vlog.Index,
-		}
+			block, err := client.BlockByHash(ctx, vlog.BlockHash)
+			if err != nil {
+				log.Printf("[ERROR]		Failed to retrieve block: %v", err)
+				return
+			}
 
-		// Serialize the data (Go Data ==> JSON)
-		serializedData, err := json.Marshal(data)
-		if err != nil {
-			log.Fatalf("[ERROR]        Failed to serialize data: %v", err)
-		}
+			data := EventData{
+				L1RootInfo: string(vlog.Data),
+				Blocktime:  time.Unix(int64(block.Time()), 0),
+				ParentHash: block.ParentHash(),
+				LogIndex:   vlog.Index,
+			}
 
-		// Store data in RedisDB
-		err = rdb.Set(ctx, strconv.Itoa(index), serializedData, 0).Err()
-		if err != nil {
-			log.Fatalf("[ERROR]        Failed to store data in Redis: %v", err)
-		}
+			serializedData, err := json.Marshal(data)
+			if err != nil {
+				log.Printf("[ERROR]        Failed to serialize data: %v", err)
+				return
+			}
+
+			key := strconv.Itoa(index)
+			pipe.Set(ctx, key, serializedData, 0) // CHANGED: Add to pipeline instead of setting directly
+
+			if (index+1)%batchSize == 0 || index == len(logs)-1 {
+				_, err = pipe.Exec(ctx) // CHANGED: Execute the pipeline in batches
+				if err != nil {
+					log.Printf("[ERROR]        Failed to execute pipeline: %v", err)
+				}
+			}
+		}(vlog, index)
 
 		index++
 	}
+
+	wg.Wait() // CHANGED: Wait for all goroutines to finish
 
 	log.Println("|=================================|")
 	log.Println("| All events stored successfully. |")
 	log.Println("|=================================|")
 
-	retrieve(redisHost, redisPort, redisPassword)
+	retrieve(rdb)
 }
 
-func retrieve(redisHost, redisPort, redisPassword string) {
-	// Connect to Redis
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     redisHost + ":" + redisPort,
-		Password: redisPassword,
-		DB:       0,
-	})
-
-	// Fetch and print all keys and values
+func retrieve(rdb *redis.Client) {
 	keys, err := rdb.Keys(ctx, "*").Result()
 	if err != nil {
 		log.Fatalf("Failed to fetch keys: %v", err)
 	}
 
 	log.Printf("Found <%d> keys in Redis\n", len(keys))
-
-	//// Uncomment below code if you want to see key, value pairs being printed.
-
-	// for _, key := range keys {
-	// 	val, err := rdb.Get(ctx, key).Result()
-	// 	if err != nil {
-	// 		log.Fatalf("Failed to fetch value for key %s: %v", key, err)
-	// 	}
-	// 	log.Printf("Key: %s, Value: %s\n", key, val)
-	// }
 }
 
 func TestDatabase(rdb *redis.Client) {
